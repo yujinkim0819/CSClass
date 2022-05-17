96d789fae7d9fe88a62a0335149a515f97583ebd
Yujin Kim <a76363282@gmail.com>
2022-05-17 12:10:58 +0900

    #2 Random 클래스의 Next()와 NextDouble()메서드에 대해서 배웠습니다

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -10,10 +10,22 @@ namespace CSClass
     {
         static void Main(string[] args)
         {
-            Car car = new Car();
-            
+            Car car = new Car();            
             car.SetInTime(); // 메소드 첫 글자는 대문자
             car.SetOutTime();
+
+            //인스턴스 메서드
+            Random random = new Random();
+            Console.WriteLine(random.Next(10, 100)); // 10부터 100 미만의 값
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble() * 10);
         }
     }
 }
