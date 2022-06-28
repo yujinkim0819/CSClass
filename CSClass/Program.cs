519c853252dffb3d6df44d1f616e4e757813a6dd
Yujin Kim <a76363282@gmail.com>
2022-06-28 10:54:29 +0900

    #22 #23 is 키워드

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -179,6 +179,35 @@ namespace CSClass
 
             Cat c = new Cat();
             Dog d = new Dog();
+
+
+
+            List<Dog> Dogs = new List<Dog>() { new Dog(), new Dog(), new Dog() };
+            List<Cat> Cats = new List<Cat>() { new Cat(), new Cat(), new Cat() };
+            List<Animal> Animals = new List<Animal>() {
+                new Dog(), new Dog(), new Dog(),
+                new Cat(), new Cat(), new Cat()
+            };
+            foreach (var item in Dogs)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Bark();
+            }
+            foreach (var item in Cats)
+            {
+                item.Eat();
+                item.Sleep();
+                item.Meow();
+            }
+            foreach (var item in Animals)
+            {
+                item.Eat();
+                item.Sleep();
+                if (item is Dog) { ((Dog)item).Bark(); }
+                if (item is Cat) { ((Cat)item).Meow(); }
+            }
+
         }
     }
 }
