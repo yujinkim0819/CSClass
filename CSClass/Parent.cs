c9a9c29ef0f38536a7afcc8f6e022bd271c8a5ce
Yujin Kim <a76363282@gmail.com>
2022-06-28 11:16:58 +0900

    #25 상속의 생성자

---
+++ CSClass/Parent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSClass
+{
+    public class Parent
+    {
+        public Parent()
+        {
+            Console.WriteLine("Parent()");
+        }
+
+        public Parent(int param)
+        {
+            Console.WriteLine("Parent(int Param)");
+        }
+
+        public Parent(string input)
+        {
+            Console.WriteLine("Parent(string input)");
+        }
+    }
+}
--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -213,6 +213,8 @@ namespace CSClass
                 if (cat != null) { cat.Meow(); }
             }
 
+            Child ChildA = new Child();
+            Child ChildB = new Child("string");
         }
     }
 }
